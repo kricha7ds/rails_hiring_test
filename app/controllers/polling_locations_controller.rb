d012b381eea8d10416ba231111f8f73898523780
Katherine Richards <kricha7ds@gmail.com>
2024-10-27 13:22:01 -0600

    Add PollingLocation controller, turbo frames for inline CRUD

---
+++ app/controllers/polling_locations_controller.rb
@@ -0,0 +1,76 @@
+class PollingLocationsController < ApplicationController
+  before_action :set_riding
+  before_action :set_polling_location, only: %i[ show edit update destroy ]
+
+  # GET /polling_location or /polling_location.json
+  def index
+    @polling_locations = @riding.polling_locations
+  end
+
+  # GET /polling_location/1 or /polling_location/1.json
+  def show
+    @polling_location
+  end
+
+  # GET /polling_location/new
+  def new
+    @polling_location = @riding.polling_locations.build
+  end
+
+  # GET /polling_location/1/edit
+  def edit
+  end
+
+  # POST /polling_location or /polling_location.json
+  def create
+    @polling_location = @riding.polling_locations.build(polling_location_params)
+
+    respond_to do |format|
+      if @polling_location.save
+        format.html { redirect_to riding_url(@riding), notice: "Polling location was successfully created." }
+        format.json { render :show, status: :created, location: @polling_location }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @polling_location.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /polling_location/1 or /polling_location/1.json
+  def update
+    respond_to do |format|
+      if @polling_location.update(polling_location_params)
+        format.html { redirect_to riding_url(@riding), notice: "Polling location was successfully updated." }
+        format.json { render :show, status: :ok, location: @polling_location }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @polling_location.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /polling_location/1 or /polling_location/1.json
+  def destroy
+    @polling_location.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to riding_url(@riding), notice: "Polling location was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_riding
+      @riding = Riding.find(params[:riding_id])
+    end
+
+    def set_polling_location
+      @polling_location = @riding.polling_locations.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def polling_location_params
+      params.require(:polling_location).permit(:title, :address, :city, :postal_code, poll_ids: [])
+    end
+end
